19f412026091f939082495b8498e4475a4116efd
eonil <pub.dev@eonil.com>
2019-06-29 20:25:01 +0900

    Refactor PDListTreeRepository.Step more clear.

--- PD/PDListTreeRepository.swift
+++ PD/PDListTreeRepository.swift
@@ -36,22 +36,14 @@ public extension PDListTreeRepository {
         let s1 = x1?.new.snapshot ?? Snapshot()
         var s2 = s1
         s2[p].value = v
-
         let t1 = x1?.new.time ?? PDTimestamp()
         let t2 = PDTimestamp()
-        let r1 = p.last!..<p.last!+1
-        let r2 = p.last!..<p.last!+1
         let x2 = Step(
-            old: Step.Point(
-                time: t1,
-                snapshot: s1,
-                path: p.dropLast(),
-                range: r1),
-            new: Step.Point(
-                time: t2,
-                snapshot: s2,
-                path: p.dropLast(),
-                range: r2))
+            operation: .setValues,
+            path: p.dropLast(),
+            range: p.last!..<p.last!+1,
+            old: Step.Point(time: t1, snapshot: s1),
+            new: Step.Point(time: t2, snapshot: s2))
         impl.record(x2)
     }
     mutating func insert(contentsOf t: ListTree<Value>, at p: IndexPath) {
@@ -59,22 +51,14 @@ public extension PDListTreeRepository {
         let s1 = x1?.new.snapshot ?? Snapshot()
         var s2 = s1
         s2.insert(contentsOf: t, at: p)
-
         let t1 = x1?.new.time ?? PDTimestamp()
         let t2 = PDTimestamp()
-        let r1 = p.last!..<p.last!
-        let r2 = p.last!..<p.last!+1
         let x2 = Step(
-            old: Step.Point(
-                time: t1,
-                snapshot: s1,
-                path: p.dropLast(),
-                range: r1),
-            new: Step.Point(
-                time: t2,
-                snapshot: s2,
-                path: p.dropLast(),
-                range: r2))
+            operation: .insertSubtrees,
+            path: p.dropLast(),
+            range: p.last!..<p.last!+1,
+            old: Step.Point(time: t1, snapshot: s1),
+            new: Step.Point(time: t2, snapshot: s2))
         impl.record(x2)
     }
     mutating func insert(_ v: Value, at p: IndexPath) {
@@ -86,40 +70,44 @@ public extension PDListTreeRepository {
         let s1 = x1?.new.snapshot ?? Snapshot()
         var s2 = s1
         s2.remove(at: p)
-
         let t1 = x1?.new.time ?? PDTimestamp()
         let t2 = PDTimestamp()
-        let r1 = p.last!..<p.last!+1
-        let r2 = p.last!..<p.last!
         let x2 = Step(
-            old: Step.Point(
-                time: t1,
-                snapshot: s1,
-                path: p.dropLast(),
-                range: r1),
-            new: Step.Point(
-                time: t2,
-                snapshot: s2,
-                path: p.dropLast(),
-                range: r2))
+            operation: .removeSubtrees,
+            path: p.dropLast(),
+            range: p.last!..<p.last!+1,
+            old: Step.Point(time: t1, snapshot: s1),
+            new: Step.Point(time: t2, snapshot: s2))
         impl.record(x2)
     }
 }
 public extension PDListTreeRepository {
     struct Step: PDTimelineStepProtocol {
         public typealias Snapshot = PDListTreeRepository.Snapshot
+        /// Operation performed in this step.
+        public var operation = Operation.setValues
+        /// Path to parent tree of operation.
+        public var path = IndexPath()
+        /// Range of operation.
+        /// For replacements, this range is about for both point snapshots.
+        /// For insertions, this range is about new point snapshot.
+        /// For removings, this range ia about old point snapshot.
+        public var range = 0..<0
         public var old = Point()
         public var new = Point()
+
+        public func reversed() -> PDListTreeRepository<Value>.Step {
+            return Step(old: new, new: old)
+        }
         public struct Point: PDTimelineStepPointProtocol {
             public var time = PDTimestamp()
             public var snapshot = PDListTreeRepository.Snapshot()
-            /// Path to parent tree.
-            public var path = IndexPath()
-            /// Selected range in parent tree.
-            public var range = 0..<0
         }
-        public func reversed() -> PDListTreeRepository<Value>.Step {
-            return Step(old: new, new: old)
+        public enum Operation {
+            case setValues
+            case insertSubtrees
+            case replaceSubtrees
+            case removeSubtrees
         }
     }
 }
