0f05fef7801cb29263d43ce60a70ff71f6039cb7
eonil <pub.dev@eonil.com>
2019-07-04 17:05:43 +0900

    Update PDAnyList.

--- PD/Utilities/PDAnyList.swift
+++ PD/Utilities/PDAnyList.swift
@@ -9,13 +9,18 @@
 public struct PDAnyList<Element>: RandomAccessCollection {
     private let r: Range<Int>
     private let sfx: (Int) -> Element
-    init<C>(_ c:C) where C:RandomAccessCollection, C.Index == Int, C.Element == Element {
-        r = c.startIndex..<c.endIndex
-        sfx = { c[$0] }
+    public init() {
+        r = 0..<0
+        sfx = { _ in fatalError() }
     }
 }
 public extension PDAnyList {
     typealias Index = Int
+    init<C>(_ c:C) where C:RandomAccessCollection, C.Index == Int, C.Element == Element {
+        r = 0..<c.count
+        let i = c.startIndex
+        sfx = { c[i+$0] }
+    }
     var startIndex: Int { return r.startIndex }
     var endIndex: Int { return r.endIndex }
     subscript(_ i: Int) -> Element { return sfx(i) }
